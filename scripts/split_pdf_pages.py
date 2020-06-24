679d6f7d319e90617716be443cfe98a760a849e3
Andrew Fitzgerald <andrew.fitzgerald@indico.io>
2020-06-24 16:21:25 -0400

    start split pdf script

---
+++ scripts/split_pdf_pages.py
@@ -0,0 +1,5 @@
+"""
+In the customer provided dataset, each page of a multipage document contains a
+single invoice sample. The app does not currently support pdf splitting during
+labeling and needs to be done upload
+"""
